2b8858a4252bc0c0f5332446756fb085f2499e97
Andi-Dwiki <andidwiki90@gmail.com>
2023-01-26 10:23:39 +0700

    update say hello with parameter name

--- say_hello.go
+++ say_hello.go
@@ -2,9 +2,12 @@
 	push module ini pada github menggunakan git bsh/ terminal lau membuat tag v1.0.0
 
 go get (nama module go-say-hello)
+dependensi
+upgrade module
+upgrade dependensi
 */
 package gosayhello
 
-func Sayhello() string {
-	return "Hello"
+func Sayhello(name string) string {
+	return "Hello world" + name
 }
